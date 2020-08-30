90bd6d5de5ef27fd90dc4bff5db2c501e3dfd4b7
Roman Solianko <RSolianko@yandex.ru>
2020-08-30 15:39:55 +0300

    Initial commit of the project

---
+++ src/test/java/ru/solianko/studyspringboot/StudyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solianko.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
